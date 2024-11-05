9db63ca2ab234a3bbc3ecde58d0877fd2c2fe18f
Hilding <98906161+HildingL@users.noreply.github.com>
2024-11-05 20:50:13 +0100

    Update randomtime_blink.ino

--- randomtime_blink.ino
+++ randomtime_blink.ino
@@ -1,10 +1,9 @@
 int randomtime; //crate the variable randomtime
 
-
 void setup() {
   // put your setup code here, to run once:
 pinMode(13, OUTPUT); //set pin 13 as output
-randomtime = random(5, 10000); //genrate a random time between 50 and 10000 milliseconds
+randomtime = random(50, 5000); //genrate a random time between 50 and 1000 milliseconds
 }
 
 void loop() {
@@ -13,4 +12,10 @@ digitalWrite(13, HIGH); //set pin 13 high/on
 delay(randomtime); //wait the random time
 digitalWrite(13, LOW); //set pin 13 low/off
 delay(randomtime); //wait the random time
+digitalWrite(13, HIGH); //set pin 13 high/on
+delay(randomtime); //wait the random time
+digitalWrite(13, LOW); //set pin 13 low/off
+delay(randomtime); //wait the random time
+randomtime = random(50, 5000); //genrate a new random time between 50 and 1000 milliseconds
 }
+
